d765864ad85a17107961ab7b31d79160ebd36737
Leon <leon.andreasen@skola.botkyrka.se>
2021-01-18 15:16:21 +0100

    part2

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-const secretMessage = "ATTACKATSEVEN"
 
 //console.log(secretMessage.charCodeAt(0))
 //console.log(secretMessage[i], charCode, charCode + 1)}
 
-//översätt till nummer
-const charCodes = []
-for (let i = 0; i < secretMessage.length; i++) {    
-charCodes.push(secretMessage[i].charCodeAt(0))
+
+
+
+function caesarEncrypt(message,shift){
+    //översätt tecken till nummer
+    const charCodes = []
+    for (let i = 0; i < message.length; i++) {    
+        charCodes.push(message[i].charCodeAt(0))
+        }
+        //shifta numret
+        for (let i = 0; i < charCodes.length; i++) {
+        charCodes[i] = charCodes[i] + shift
+        }
+        
+        let result = ""
+        //översätt tillbaka från charcode
+        for (let i = 0; i < charCodes.length; i++) {
+        result = result + String.fromCharCode(charCodes[i])    
+        }
+        return result
 }
-//shifta numret
+const secretMessage = "Hellofriends"
+const encryptedmessage = caesarEncrypt(secretMessage, 2)
+console.log(secretMessage, encryptedmessage)
+
+function caesarDecrypt(encryptedmessage,shift){
+    const charCodes = []
+    for (let i = 0; i < encryptedmessage.length; i++) {
+        charCodes.push(encryptedmessage[i].charCodeAt(0))
+        
+    }
 for (let i = 0; i < charCodes.length; i++) {
-charCodes[i] = charCodes[i] + 1
-}
-
-
-console.log(charCodes)
-
-let encryptedmessage = ""
+    charCodes[i] = charCodes[i] - shift
+    
+}            
+let result = ""
 //översätt tillbaka från charcode
 for (let i = 0; i < charCodes.length; i++) {
-encryptedmessage = encryptedmessage + String.fromCharCode(charCodes[i])    
+result = result + String.fromCharCode(charCodes[i])    
 }
-console.log(encryptedmessage)
-
-function ceasarEncrypt(message,shift){
-
-}
-
-function ceasarDecrypt(encryptedmessage,shift){
-
-}
+return result
+        }
+        const decryptedmessage = caesarDecrypt(encryptedmessage, 2)
+console.log(secretMessage, decryptedmessage)
+//const decryptedmessage = caesarDecrypt()
+//console.log(encryptedmessage, decryptedmessage)
